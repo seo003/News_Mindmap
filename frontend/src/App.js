061c4bf4da4892bbbca719097ec45550d442e169
seo003 <sy030212@naver.com>
2025-04-12 20:19:48 +0900

    Add loading UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000); 
+  }, []);
+
+  // 뉴스 크롤링 및 분석 로딩 화면
+  if (loading) {
+    return (
+      <section class="loading">
+      <h1 class="loading-title">뉴스정보 가져오는 중...</h1>
+      <div class="progress-bar" aria-hidden="true">
+        <span class="progress-bar-gauge"></span>
+      </div>
+      </section>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>마인드맵</h1>
     </div>
   );
 }
